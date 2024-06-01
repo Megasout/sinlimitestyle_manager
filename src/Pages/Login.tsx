32599f9318e5cccc64061347783fcae1e19372b0
Megasout <Megasoutt@gmail.com>
2024-06-01 01:18:12 -0300

    Login autocomplete fixed

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,6 +1,6 @@
 import { Form } from "react-router-dom"
 import "../Styles/login.scss"
-import React, { useRef, useState } from "react"
+import React, { ChangeEvent, useEffect, useRef, useState } from "react"
 
 function Login() {
     return (
@@ -12,7 +12,7 @@ function Login() {
                         <InputLogin text="Dirección de email" type="email" />
                         <InputLogin text="Contraseña" type="password" />
                     </div>
-                    <input type="submit" value={"ACCEDER"}/>
+                    <input type="submit" value={"ACCEDER"} />
                 </Form>
             </div>
         </div>
@@ -27,23 +27,34 @@ type InputLoginType = {
 }
 
 function InputLogin(props: InputLoginType) {
-    const { text } = props
-    const inputRef = useRef<HTMLInputElement>(null)
+    const { text, type } = props
+    // const inputRef = useRef<HTMLInputElement>(null)
+    const [value, setValue] = useState<string | undefined>()
     const [labelPlaceholder, setLabel] = useState(true)
+
+    useEffect(() => {
+        if(value != '')
+            setLabel(false)
+    }, [value])
 
     const handleOnFocus = () => {
         setLabel(false)
     }
 
     const handleOnBlur = () => {
-        if (inputRef.current?.value === '')
+        // if (inputRef.current?.value === '')
+        if (value == '')
             setLabel(true)
+    }
+
+    const handleOnChange = (event: any) => {
+        setValue(event.target.value)
     }
 
     return (
         <div style={{ position: "relative" }}>
             <label className={`${!labelPlaceholder ? 'placeholder' : ''}`}>{text}</label>
-            <input ref={inputRef} onBlur={handleOnBlur} onFocus={handleOnFocus} type="email" />
+            <input onBlur={handleOnBlur} onFocus={handleOnFocus} type={type} value={value} onChange={handleOnChange} />
         </div>
     )
 }
