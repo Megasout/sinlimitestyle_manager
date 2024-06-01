fec7747955d92d2adc1798f1ab8d924df6839c01
Megasout <Megasoutt@gmail.com>
2024-05-31 23:25:18 -0300

    Login Form

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,9 +1,49 @@
+import { Form } from "react-router-dom"
+import "../Styles/login.scss"
+import React, { useRef, useState } from "react"
+
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <div className="block">
+                <h1>Acceder</h1>
+                <Form>
+                    <div className="info">
+                        <InputLogin text="Dirección de email" type="email" />
+                        <InputLogin text="Contraseña" type="password" />
+                    </div>
+                    <input type="submit" value={"ACCEDER"}/>
+                </Form>
+            </div>
         </div>
     )
 }
 
 export default Login
+
+type InputLoginType = {
+    text: string,
+    type: React.HTMLInputTypeAttribute
+}
+
+function InputLogin(props: InputLoginType) {
+    const { text } = props
+    const inputRef = useRef<HTMLInputElement>(null)
+    const [labelPlaceholder, setLabel] = useState(true)
+
+    const handleOnFocus = () => {
+        setLabel(false)
+    }
+
+    const handleOnBlur = () => {
+        if (inputRef.current?.value === '')
+            setLabel(true)
+    }
+
+    return (
+        <div style={{ position: "relative" }}>
+            <label className={`${!labelPlaceholder ? 'placeholder' : ''}`}>{text}</label>
+            <input ref={inputRef} onBlur={handleOnBlur} onFocus={handleOnFocus} type="email" />
+        </div>
+    )
+}
